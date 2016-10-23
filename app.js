596829d0cd786f8fff171793506e9c2ac8f349a7
Addy Kim <soyaddyk@gmail.com>
2016-10-23 11:36:32 -0500

    Try 1

--- app.js
+++ app.js
@@ -15,14 +15,16 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('my_voice_is_my_password_verify_me')
+    // res.send(req.query['hub.challenge'])
+    // res.send('Hello world, I am a chat bot')
 })
 
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+      res.send('my_voice_is_my_password_verify_me')
     }
     res.send('Error, wrong token')
 })
